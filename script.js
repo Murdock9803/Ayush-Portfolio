14833ffad323764a9a48660c8ec0296bc54f5af0
Murdock9803 <ayushsahu9803@gmail.com>
2025-09-11 07:57:24 +0530

    added scriptJs and shifted js code there

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+    strings: [
+        "API/devOPS'er",
+        "UI/UX Designer",
+        "Technical Writer",
+        "Freelance Dev",
+        "Data Engineer",
+        "Software Developer"
+    ],
+    typeSpeed: 75,
+});
