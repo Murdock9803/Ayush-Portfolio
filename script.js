3b5d28348c5e5a9639bd1bb5c39c23a2eb280a46
Murdock9803 <ayushsahu9803@gmail.com>
2025-09-11 08:19:20 +0530

    done with the dynamic year fn. in footer

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Typed.js details for landing page
-var typed = new Typed("#element", {
-    strings: [
-        "API/devOPS'er",
-        "UI/UX Designer",
-        "Technical Writer",
-        "Freelance Dev",
-        "Data Engineer",
-        "Software Developer"
-    ],
-    typeSpeed: 75,
-});
-
 // Dynamic fetching year in footer
-const currYear = document.querySelector('#currYear');
+const currYear = document.querySelector('.currYear');
 currYear.textContent = new Date().getFullYear();
